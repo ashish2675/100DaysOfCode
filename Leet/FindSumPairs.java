0ec36a07728c59e5ace14a6e90c0cefa19485ce0
Ashish Jose <80691110+ashish2675@users.noreply.github.com>
2023-10-28 19:43:01 +0530

    Create FindSumPairs.java

---
+++ Leet/FindSumPairs.java
@@ -0,0 +1,26 @@
+//nt
+class FindSumPairs {
+    Map<Integer, Integer> map = new HashMap<>();
+    int[] nums1, nums2;
+
+    public FindSumPairs(int[] nums1, int[] nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        for(int num: nums2) 
+            map.put(num, map.getOrDefault(num, 0) + 1);
+    }
+    
+    public void add(int index, int val) {
+        map.put(nums2[index], map.get(nums2[index]) - 1);
+        nums2[index] += val;
+        map.put(nums2[index], map.getOrDefault(nums2[index], 0) + 1);
+    }
+    
+    public int count(int tot) {
+        int result =0;
+        for(int num: nums1) 
+            if(map.containsKey(tot - num)) 
+                result += map.get(tot - num);
+        return result;
+    }
+}

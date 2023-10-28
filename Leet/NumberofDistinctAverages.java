0c791f3f20dd8c0cb36503a27f072b49c2676e8a
Ashish Jose <80691110+ashish2675@users.noreply.github.com>
2023-10-28 23:04:00 +0530

    Create NumberofDistinctAverages.java

---
+++ Leet/NumberofDistinctAverages.java
@@ -0,0 +1,12 @@
+//nt
+class Solution {
+    public int distinctAverages(int[] nums) {
+        Arrays.sort(nums);
+		Set<Double> s=new TreeSet<>();
+		for(int i=0,j=nums.length-1;i<j;i++,j--) {
+			double avg=(double)(nums[i]+nums[j])/2;
+			s.add(avg);
+		}
+		return s.size();
+    }
+}

e9ef87c5564afb2ded647955f982678c08965727
Ashish Jose <80691110+ashish2675@users.noreply.github.com>
2023-10-29 22:41:55 +0530

    Create Poor Pigs.java

---
+++ Leet/Poor Pigs.java
@@ -0,0 +1,14 @@
+//nt
+class Solution
+{
+    public int poorPigs(int buckets, int tdie, int ttest)
+    {
+        int test = ttest/tdie;
+        int i=0;
+        while(Math.pow(test+1,i)< buckets)
+        {
+            i++;
+        }
+        return i;
+    }
+}

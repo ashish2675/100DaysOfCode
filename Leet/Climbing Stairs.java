472f9741f82218f66689b35d897eb14bf5762578
Ashish Jose <80691110+ashish2675@users.noreply.github.com>
2023-10-29 23:59:03 +0530

    Update Climbing Stairs.java

--- Leet/Climbing Stairs.java
+++ Leet/Climbing Stairs.java
@@ -1,21 +1,9 @@
 //nt
 class Solution {
-public:
-    int climbStairs(int n) {
-        
-        if(n <= 2) return n;
-
-        int prev2 = 1;
-        int prev1 = 2;
-        int curr;
-
-        for(int i = 3; i <= n; i++)
-        {
-            curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
         }
-
-        return curr;
+        return climbStairs(n-1) + climbStairs(n-2);
     }
-};
+}

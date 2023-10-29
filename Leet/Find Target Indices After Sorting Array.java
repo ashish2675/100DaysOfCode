e5a4e8f04879d167fa8249df2d3cb199e27e766f
Ashish Jose <80691110+ashish2675@users.noreply.github.com>
2023-10-29 23:54:14 +0530

    Create Find Target Indices After Sorting Array.java

---
+++ Leet/Find Target Indices After Sorting Array.java
@@ -0,0 +1,13 @@
+//nt
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        List<Integer> AL = new ArrayList();
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == target) {
+                AL.add(i);
+            }
+        }
+        return AL;
+    }
+}

51aaf7f98ae340792603df2ad83264b96df072b7
ShubhamShakyawal <shakyawals@gmail.com>
2023-10-29 16:40:20 +0530

    4 Leetcode Questions Answers added

---
+++ Leet/MinimumNumberOfOperationsToMakeArrayContinuous.java
@@ -0,0 +1,27 @@
+// LeetCode Qno. 2009.
+// URL - https://leetcode.com/problems/minimum-number-of-operations-to-make-array-continuous
+
+class Solution {
+    public int minOperations(int[] nums) {
+        int len = nums.length;
+        Set<Integer> set = new HashSet();
+
+        for(int num: nums)
+            set.add(num);
+
+        Integer[] uniqueNums = new Integer[set.size()];
+        uniqueNums=set.toArray(uniqueNums);
+
+        Arrays.sort(uniqueNums, (a, b)->(a-b));
+
+        int end = 0;
+        int max = Integer.MIN_VALUE;
+        
+        for(int start = 0; start< uniqueNums.length; start++){
+            while(end <uniqueNums.length && uniqueNums[end]<uniqueNums[start]+len)
+                end++;
+            max = Math.max(max, end-start);
+        }
+        return len - max;
+    }
+}
